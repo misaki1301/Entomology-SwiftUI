031dcfef887a6c4c4ecc4ad353df09a78dd7a8a1
Paul Pacheco <paul.pacheco@pragma.com.co>
2023-07-14 01:09:26 -0500

    added new views for flat and outlined buttons with material like design

--- Entomology/Views/Components/MaterialButton.swift
+++ Entomology/Views/Components/MaterialButton.swift
@@ -22,11 +22,45 @@ struct MaterialButton: View {
 	}
 }
 
+struct MaterialFlatButton: View {
+	var id = ""
+	var text: String
+	var action: () -> Void
+	var body: some View {
+		Button(action: action) {
+			Text(text)
+				.padding(.vertical, 10)
+				.padding(.horizontal, 24)
+		}
+		.buttonStyle(MaterialFillButtonStyle())
+		.accessibilityIdentifier(id)
+	}
+}
+
+struct MaterialOutlinedButton: View {
+	var id = ""
+	var text: String
+	var action: () -> Void
+	var body: some View {
+		Button(action: action) {
+			Text(text)
+				.padding(.vertical, 10)
+				.padding(.horizontal, 24)
+		}
+		.buttonStyle(MaterialOutlinedButtonStyle())
+		.accessibilityIdentifier(id)
+	}
+}
+
 struct MaterialButton_Previews: PreviewProvider {
     static var previews: some View {
-		return MaterialButton(
-			text: "Registro",
-			action: {}
-		)
+		VStack {
+			MaterialButton(
+				text: "Registro",
+				action: {}
+			)
+			MaterialFlatButton(text: "Editar", action: {})
+			MaterialOutlinedButton(text: "Guardar", action: {})
+		}
     }
 }
