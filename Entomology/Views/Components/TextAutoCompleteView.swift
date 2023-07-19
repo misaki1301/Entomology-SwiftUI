aa6dd3f1e0d224cb5797d3b90606b25d9dbae279
Paul Pacheco <paul.pacheco@pragma.com.co>
2023-07-18 22:10:08 -0500

    refactorized TextAutoCompleteView

--- Entomology/Views/Components/TextAutoCompleteView.swift
+++ Entomology/Views/Components/TextAutoCompleteView.swift
@@ -14,6 +14,7 @@ struct MenuItem: View {
 			.font(.custom("Roboto-Regular", size: 16))
 			.padding(.horizontal, 16)
 			.padding(.vertical, 8)
+			//.frame(height: 56)
 	}
 }
 
@@ -22,9 +23,9 @@ struct ClearButton: ViewModifier {
 	func body(content: Content) -> some View {
 		ZStack(alignment: .trailing) {
 			content
-			
+
 			if !text.isEmpty {
-				Button(action: {text = ""}) {
+				Button(action: { text = "" }) {
 					Image(systemName: "multiply.circle.fill")
 						.foregroundStyle(.gray)
 				}
@@ -43,49 +44,61 @@ extension View {
 struct TextAutoCompleteView: View {
 	@FetchRequest
 	private var insects: FetchedResults<Insect>
-	@State var text = ""
-	@State var items = ["Abeja", "Hormiga"]
 	
-	init(text: String = "", items: [String] = ["Abeja", "Hormiga"]) {
-		_insects =  FetchRequest(fetchRequest: Insect.searchByName(searchTerm: text))
-		self.text = text
-		self.items = items
+	@Binding var text: String
+	@Binding var selected: Insect?
+	@State private var isAnimated = false
+	
+	init(text: Binding<String>, selected: Binding<Insect?>) {
+		self._selected = selected
+		self._text = text
+		_insects = FetchRequest(fetchRequest: Insect.searchByName(searchTerm: text.wrappedValue))
 	}
-	
-    var body: some View {
-		ZStack(alignment: .leading) {
-			VStack(alignment: .leading, spacing: 0) {
-				TextField("Nombre de la especie", text: $text)
-					.frame(width: 200, height: 56)
-					.padding(.horizontal, 16)
-					.clearButton(text: $text)
-					.overlay {
-						RoundedRectangle(cornerRadius: 4)
-							.inset(by: 1)
-							.stroke(Color(red: 0, green: 0.43, blue: 0.19), lineWidth: 2)
-					}
-				VStack(spacing: 0) {
-					LazyVStack(alignment: .leading, spacing: 0) {
-						ForEach(insects, id: \.self) { item in
-							MenuItem(text: item.speciesName ?? "")
+
+	var body: some View {
+		ZStack(alignment: .top) {
+			TextField("Nombre de la especie", text: $text)
+				.frame(width: 200, height: 56)
+				.padding(.horizontal, 16)
+				.clearButton(text: $text)
+				.overlay {
+				RoundedRectangle(cornerRadius: 4)
+					.inset(by: 1)
+					.stroke(Color(red: 0, green: 0.43, blue: 0.19), lineWidth: 2)
+				}
+				.overlay(alignment: .topLeading) {
+					VStack {
+						LazyVStack(alignment: .leading, spacing: 0) {
+							ForEach(insects, id: \.self) { item in
+								MenuItem(text: item.speciesName ?? "")
+							}
+							NavigationLink(destination: CreateInsectView()) {
+								MenuItem(text: "Otro")
+							}
 						}
-						//this redirects to create insect
-						MenuItem(text: "Otro")
-						//Text("\(insects.count)")
+						.frame(width: 232)
+						.background(Color("card_background"))
+						.cornerRadius(4)
+						.shadow(radius: 2)
+						.offset(x: 0, y: 56)
+						.zIndex(200)
+						.opacity((!text.isEmpty || !insects.isEmpty) ? 1 : 0)
+					}.onAppear {
+						withAnimation(isAnimated ? .easeIn : .easeOut) {
+							self.isAnimated.toggle()
+						}
 					}
 				}
-				.frame(width: 200)
-				.background(Color("card_background"))
-				.cornerRadius(4)
-				.shadow(radius: 2)
+				.zIndex(200)
 			}
-		}
-    }
+	}
 }
 
 struct TextAutoCompleteView_Previews: PreviewProvider {
-    static var previews: some View {
-		TextAutoCompleteView(text: "").backgroundColor(Color("background"))
+	static var previews: some View {
+		let context = CoreDataProvider.preview.persistentContainer.viewContext
+		TextAutoCompleteView(text: .constant("mor"), selected: .constant(Insect(context: context)))
+			.backgroundColor(Color("background"))
 			.environment(\.managedObjectContext, CoreDataProvider.preview.persistentContainer.viewContext)
-    }
+	}
 }
