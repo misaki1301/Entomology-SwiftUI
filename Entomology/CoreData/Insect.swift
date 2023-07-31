9e46b11d7121cc87fc8946c7449cc891a1087ca7
misaki1301 <paul.frankpc@gmail.com>
2023-07-31 15:16:53 -0500

    Added query to group and count by insects species name by each record count added by the user

--- Entomology/CoreData/Insect.swift
+++ Entomology/CoreData/Insect.swift
@@ -36,4 +36,49 @@ extension Insect: Model {
 		request.predicate = NSPredicate(format: "speciesName BEGINSWITH[c] %@", searchTerm)
 		return request
 	}
+	
+	static func countBySpecie() -> NSFetchRequest<Insect> {
+		let request = Insect.fetchRequest()
+		request.sortDescriptors = []
+		let sumExp = NSExpressionDescription()
+		sumExp.name = "total_count"
+		sumExp.expression = NSExpression(format: "@sum.countRecords.count")
+		sumExp.expressionResultType = .integer32AttributeType
+		
+		request.propertiesToFetch = ["speciesName", sumExp]
+		
+		request.propertiesToGroupBy = ["speciesName"]
+		
+		request.resultType = .dictionaryResultType
+		
+		return request
+	}
+	
+	static func fetchTotalCountPerInsect() -> [NSDictionary]? {
+		let fetchRequest = NSFetchRequest<NSDictionary>(entityName: "Insect")
+
+		// Define the SUM expression for the CountRecord's "count" attribute
+		let sumExpression = NSExpressionDescription()
+		sumExpression.name = "total_count"
+		sumExpression.expression = NSExpression(format: "@sum.countRecords.count")
+		sumExpression.expressionResultType = .integer32AttributeType
+
+		// Set the properties to fetch (insect's attributes and the calculated sum)
+		fetchRequest.propertiesToFetch = ["speciesName", "urlPhoto", "localePhoto", "locate", "moreInfoUrl", sumExpression]
+
+		// Group by the Insect entity
+		fetchRequest.propertiesToGroupBy = ["speciesName"]
+
+		// Specify the result format
+		fetchRequest.resultType = .dictionaryResultType
+
+		do {
+			let context = CoreDataProvider.currentContext
+			let result = try context.fetch(fetchRequest)
+			return result
+		} catch {
+			print("Error fetching data: \(error)")
+			return nil
+		}
+	}
 }
